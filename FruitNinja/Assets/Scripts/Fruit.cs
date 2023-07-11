1be753ff79dbff983dfd7d22063513d1050928cf
Valentin Chivliev <valentin.chivliev02@gmail.com>
2023-07-11 17:15:25 +0300

    Create Fruits And Explode Them

---
+++ FruitNinja/Assets/Scripts/Fruit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruitPrefab;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+        Rigidbody[] rbsOnSliced = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody r in rbsOnSliced)
+        {
+            r.transform.rotation = Random.rotation;
+            r.AddExplosionForce(Random.Range(500,1000),transform.position,5);
+        }
+        Destroy(gameObject);
+
+    }
+}
