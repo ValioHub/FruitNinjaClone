9bd264950a0f6bc2a2dc99172a39efa0ab6f3e48
Valentin Chivliev <valentin.chivliev02@gmail.com>
2023-07-11 19:38:58 +0300

    Update Blade.cs

--- FruitNinja/Assets/Scripts/Blade.cs
+++ FruitNinja/Assets/Scripts/Blade.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public float minVelo = 0.1f;
+    
     private Rigidbody2D rb;
+    private Vector3 lastMousePos;
+    private Vector3 mouseVelo;
+
+    private Collider2D col;
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         SetBladeToMouse();
+    }
+    private void FixedUpdate()
+    {
+        col.enabled = isMouseMoving();
     }
     private void SetBladeToMouse()
     {
@@ -23,5 +34,20 @@ public class Blade : MonoBehaviour
         mousePos.z = 10;
 
         rb.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+    private bool isMouseMoving()
+    {
+        Vector3 curMousePos = transform.position;
+        float traveled = (lastMousePos - curMousePos).magnitude;
+        lastMousePos = curMousePos;
+
+        if (traveled > minVelo)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
